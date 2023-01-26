18c23eb669045d4af627b65dd6d7dd708d7678e5
ishrat-jahan-swarna <120089436+ishrat-jahan-swarna@users.noreply.github.com>
2023-01-27 00:49:40 +0600

    Update welcome_student_controller.rb

--- app/controllers/welcome_student_controller.rb
+++ app/controllers/welcome_student_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeStudentController < ApplicationController
   before_action :authorize_user
   skip_before_action  :authorize
+
+  @@selected_courses = []
+
   def index
     cuser = User.find_by(id: session[:user_id])
     @user = cuser
@@ -11,5 +14,23 @@ class WelcomeStudentController < ApplicationController
 
   def available_courses
     @user = User.find(params[:u_id])
+  end
+
+  def sel_to_enroll
+    @course = Course.find(params[:course_id])
+    @@selected_courses.append(@course)
+  end
+
+  def enroll_courses
+    @user = User.find(params[:u_id])
+    @total_credit = sum(@@selected_courses.credit)
+    puts @total_credit
+    if @@selected_courses >= 6 && @total_credit < 40
+      @user.courses.push(@@selected_courses)
+      @@selected_courses = []
+    else
+      @@selected_courses = []
+    end
+    redirect_to welcome_student_index_path
   end
 end
