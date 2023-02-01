f4d7d65519fe21d666f4521084b3ab226ef190a7
ishrat-jahan-swarna <120089436+ishrat-jahan-swarna@users.noreply.github.com>
2023-02-01 22:45:08 +0600

    Update courses_controller.rb

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -77,6 +77,6 @@ class CoursesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def course_params
-      params.require(:course).permit(:code, :title, :description, :prerequisite, :credit, :days, :semester)
+      params.require(:course).permit(:code, :title, :description, :prerequisite, :credit, :days, :semester, :option)
     end
 end
