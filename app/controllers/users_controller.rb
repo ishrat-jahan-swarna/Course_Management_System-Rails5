3788adf7430e18c0a67abee3e733add2105f16e5
ishrat-jahan-swarna <ishrat@nascenia.com>
2023-02-08 13:07:29 +0600

    8 Feb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -96,6 +96,14 @@ class UsersController < ApplicationController
     redirect_to @user
   end
 
+  def reject_user
+    @user = User.find(params[:u_id])
+    @up = UserProfile.find_by(user_id: @user.id)
+    @up.approved = false
+    @up.save!
+    redirect_to @user
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
